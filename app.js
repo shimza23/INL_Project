517bb0e894892e1a794db50e306fded68f6bc1cf
ShaunvdBijl <shaunvdbijl@gmail.com>
2026-05-15 14:41:34 +0200

    added routing

---
+++ app.js
@@ -0,0 +1,24 @@
+//set all needed variables 
+const express = require('express');
+const path    = require('path');
+const app     = express();
+const dotenv  = require('dotenv').config();
+const port    = process.env.PORT || 3000;
+
+require('./connection'); // if needed
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+//set routing
+app.get('/', (req, res) => { res.render('index'); });
+app.get('/contact', (req, res) => { res.render('contact'); });
+app.get('/dashboard', (req, res) => { res.render('dashboard'); }); // fix filename too
+app.get('/manage', (req, res) => { res.render('manage'); });
+app.get('/authentication', (req, res) => { res.render('authentication'); });
+
+//set server
+app.listen(port, () => {
+  console.log(`Server is running on localhost:${port}`);
+});
